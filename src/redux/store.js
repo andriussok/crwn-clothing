dcc340dea125c8f122ab0df4ae1813eddf703cd0
Andriuss <andriussok@gmail.com>
2020-04-23 16:50:58 +0100

    added Redux Persist, to store cart items in Local Storage, added directory state and collection state into Redux, created Collection Overview component. Added nested routing in Shop Page. Collection routing and selector

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger'; // this is smething that is nice to use when debugging redux code
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger'; // this is smething that is nice to use when debugging redux code, you can use chrome extenssion
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// technically we don't need to export const's
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store, persistor};
